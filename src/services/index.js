c081a73275e0d7f6160def25c3893f76549a4bac
aobandof <aobandof.developer@gmail.com>
2019-11-23 14:27:49 -0300

    thitd commit

--- src/services/index.js
+++ src/services/index.js
@@ -2,10 +2,8 @@ import axios from 'axios';
 
 export async function getWeather(location) {
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.lng}&appid=${process.env.REACT_APP_WEATHER_KEY}&units=metric`;
-  console.log(url);
   let response = {}
   try {
-    console.log(location.lat);
     if(location.lat !== 0) {
       response = await axios({
         url,
